7f96b84351c0d00075273f0ed8ccd090cde39e2e
adamdelancey <adamdelancey@gmail.com>
2021-01-29 10:43:50 +0000

    completed homepage content and started css/js

--- home/templates/static/script.js
+++
@@ -1,7 +0,0 @@
- $(document).ready(function(){
-    //The following functions are used to initialise Materialize CSS
-    $('.sidenav').sidenav({edge: "right"});
-    $(".dropdown-trigger").dropdown();
-   
-
-});
---
+++ static/js/scripts.js
@@ -0,0 +1,7 @@
+
+     $(document).ready(function(){
+    //The following functions are used to initialise Materialize CSS
+    $('.sidenav').sidenav({edge: "right"});
+    $(".dropdown-trigger").dropdown(); 
+
+});
