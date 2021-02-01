cef04a974d5512960fa36ae454862f5bfa1942c2
adamdelancey <adamdelancey@gmail.com>
2021-02-01 08:42:39 +0000

    added tours views and templates

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Tour, Match
 
 # Create your views here.
+
+def tours(request):
+    """ A view to return the tours page, including sorting and search queries """
+
+    tours = Tour.objects.all()
+    matches = Match.objects.all()
+
+    context = {
+        'tours': tours,
+        'matches': matches
+    }
+
+    return render(request, 'tours/tours.html', context)
