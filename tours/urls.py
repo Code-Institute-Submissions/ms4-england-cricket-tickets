16a41c09b6b387cce7a35e66655a60959a63fad8
adamdelancey <adamdelancey@gmail.com>
2021-02-01 12:14:41 +0000

    created basic layout for tickets.html and added views and urls

--- tours/urls.py
+++ tours/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tours, name='tours'),
+    path('<match_id>', views.tickets, name='tickets'),
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Tour, Match
+from django.shortcuts import render, get_object_or_404
+from .models import Tour, Match, Stadium, Ticket
 
 # Create your views here.
 
@@ -15,3 +15,22 @@ def tours(request):
     }
 
     return render(request, 'tours/tours.html', context)
+
+
+def tickets(request, match_id):
+    """
+    A view to return the tickets page, showing the
+    stadium and tickets available
+    """
+
+    match = get_object_or_404(Match, pk=match_id)
+    stadiums = Stadium.objects.all()
+    tickets = Ticket.objects.all()
+
+    context = {
+        'match': match,
+        'stadiums': stadiums,
+        'tickets': tickets
+    }
+
+    return render(request, 'tours/tickets.html', context)
