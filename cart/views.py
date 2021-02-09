395c00e487692c36c6e64501d7576aa7cb6b4847
adamdelancey <adamdelancey@gmail.com>
2021-02-09 10:57:35 +0000

    added notification css, shopping cart preview and additional messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from tours.models import Ticket
 
@@ -14,7 +14,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """Add a quantity to the shopping cart"""
-    ticket = Ticket.objects.get(pk=item_id)
+    ticket = get_object_or_404(Ticket, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     day = None
@@ -27,16 +27,20 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if day in cart[item_id]['items_by_day'].keys():
                 cart[item_id]['items_by_day'][day] += quantity
+                messages.success(request, f'Update: {cart[item_id]["items_by_day"][day]} x Day {day} {ticket.friendly_name} tickets now in your cart')
             else:
                 cart[item_id]['items_by_day'][day] = quantity
+                messages.success(request, f'Added {quantity} x Day {day} {ticket.friendly_name} to your cart')
         else:
             cart[item_id] = {'items_by_day': {day: quantity}}
+            messages.success(request, f'Added {quantity} x Day {day} {ticket.friendly_name} to your cart')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request, f'Update: {cart[item_id]} x {ticket.friendly_name} tickets now in your cart')
         else:
             cart[item_id] = quantity
-            messages.error(request, f'Added {quantity} x {ticket.friendly_name} to your bag')
+            messages.success(request, f'Added {quantity} x {ticket.friendly_name} to your cart')
 
     request.session['cart'] = cart
 
@@ -46,7 +50,7 @@ def add_to_cart(request, item_id):
 def update_cart(request, item_id):
 
     """Adjust the quantity of the specified product to the specified amount"""
-
+    ticket = get_object_or_404(Ticket, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     day = None
     if 'day' in request.POST:
@@ -56,15 +60,20 @@ def update_cart(request, item_id):
     if day:
         if quantity > 0:
             cart[item_id]['items_by_day'][day] = quantity
+            messages.success(request, f'Update: {cart[item_id]["items_by_day"][day]} x Day {day} {ticket.friendly_name} tickets now in your cart')
+
         else:
             del cart[item_id]['items_by_day'][day]
             if not cart[item_id]['items_by_day']:
                 cart.pop(item_id)
+            messages.success(request, f'Removed {quantity} x Day {day} {ticket.friendly_name} from your cart')
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Update: {cart[item_id]} x {ticket.friendly_name} tickets now in your cart')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {ticket.friendly_name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -73,6 +82,7 @@ def update_cart(request, item_id):
 def remove_item(request, item_id):
     """Remove the item from the shopping cart"""
     try:
+        ticket = get_object_or_404(Ticket, pk=item_id)
         day = None
         if 'day' in request.POST:
             day = request.POST['day']
@@ -81,16 +91,21 @@ def remove_item(request, item_id):
         if day:
             del cart[item_id]['items_by_day'][day]
             if not cart[item_id]['items_by_day']:
-                cart.pop(item_id)                
+                cart.pop(item_id)
+            messages.success(request, f'Removed Day {day} {ticket.friendly_name} tickets from your cart')
         else:
             cart.pop(item_id)
-            
+            messages.success(request, f'Removed {ticket.friendly_name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
+
+
+
 
 
 
