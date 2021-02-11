dff1ef8665a2ce9e69c875d156d2a312209e2cfc
adamdelancey <adamdelancey@gmail.com>
2021-02-11 15:14:11 +0000

    added profiles app

--- ms4_england_cricket_tickets/settings.py
+++ ms4_england_cricket_tickets/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'checkout',
     'crispy_forms',
     'crispy_forms_materialize',
+    'profiles',
 ]
 
 MIDDLEWARE = [
--- ms4_england_cricket_tickets/urls.py
+++ ms4_england_cricket_tickets/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('tours/', include('tours.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
