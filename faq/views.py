c50923ef067c6a71d6f58a965f5e9d9729859ae2
adamdelancey <adamdelancey@gmail.com>
2021-02-18 08:32:57 +0000

    added faq template, tour, url and fixture

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import FAQ
 
 # Create your views here.
+
+
+def faq(request):
+    """
+    A view to return the tickets page, showing the
+    stadium and tickets available
+    """
+
+    faq = FAQ.objects.all()
+
+    context = {
+        'faq': faq
+    }
+
+    return render(request, 'faq/faq.html', context)
